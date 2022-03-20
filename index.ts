8b8f6ef5f254544edbe61f87db4fef3912630136
cybericecream <cybericecream99@gmail.com>
2022-03-20 16:24:07 +0800

    Removed OAuth authentication.

--- index.ts
+++ index.ts
@@ -5,22 +5,10 @@ import express, { Request, Response, Router, ErrorRequestHandler } from "express
 import connection from "./src/services/connection";
 import {Error} from "sequelize";
 const bodyParser = require('body-parser');
-const { auth } = require('express-oauth2-jwt-bearer');
 const dogRouter = require('./src/routes/dogRoute');
 
 const app = express()
 require('dotenv').config();
-
-// Authorization middleware. When used, the Access Token must
-// exist and be verified against the Auth0 JSON Web Key Set.
-// TODO: Add Error handling for the Following
-// UnauthorizedError
-// InvalidTokenError
-// InsufficientScopeError
-const checkJwt = auth({
-  audience: process.env.AUDIENCE,
-  issuerBaseURL: process.env.ISSUER_URL,
-});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -28,9 +16,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.get("/", (req: Request, res: Response): Response => {
   return res.json({ message: "Sequelize Example 🤟" });
 });
-
-// Any end point following requires auth to access
-app.use(checkJwt);
 
 app.use('/dogs', dogRouter);
 
--- src/routes/dogRoute.ts
+++ src/routes/dogRoute.ts
@@ -1,19 +1,11 @@
 import express, {Request, Response} from "express";
 const router = express.Router();
-const { requiredScopes } = require('express-oauth2-jwt-bearer');
 
 const dogsController = require("../controller/dogsController");
-
-const viewScopes = requiredScopes('view:dogs');
-const editScopes = requiredScopes('edit:dogs');
-
-router.use(viewScopes);
 
 router.get("/", dogsController.listDogs);
 
 router.get("/:id", dogsController.viewDogs);
-
-router.use(editScopes);
 
 router.post("/", dogsController.saveDogs);
 
